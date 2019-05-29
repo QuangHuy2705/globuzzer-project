544f446dd61bf73dcf8df101a340c6fbfe6e029e
QuangHuy <ntquanghuy2705@gmail.com>
2019-05-30 00:06:04 +0700

    fix redux-dev-tool

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,12 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import fbConfig from '../config/fbConfig'
 
-const store = createStore(rootReducer, compose(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk.withExtraArgument({ getFirebase,getFirestore })),
     reduxFirestore(fbConfig),
     reactReduxFirebase(fbConfig),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 export default store
